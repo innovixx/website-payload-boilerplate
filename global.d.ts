98dd31118ef84f44980e6d77bedda4a3e869e979
Aman Bhogal <aman.bhogal@innovixx.co.uk>
2025-03-06 09:48:03 +0000

    chore: update ESLint configuration and adjust module declarations

--- global.d.ts
+++ global.d.ts
@@ -1,3 +1,1 @@
-declare module '@innovixx/eslint-config/config/configs/base/index.mjs'
-declare module '@innovixx/eslint-config/config/configs/react/index.mjs'
-declare module '@innovixx/eslint-config/config/configs/typescript/index.mjs'
+declare module '@innovixx/eslint-config/config/configs/*/index.mjs'
